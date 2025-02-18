d0655020fc4650d15b4561d8768d0bc6c383ead2
Boris Milašinović <boris.milasinovic@fer.hr>
2025-02-18 18:31:34 +0100

    Custom resolver with enabled projection

---
+++ GraphQL/CountryResolvers.cs
@@ -0,0 +1,13 @@
+﻿using GraphQLserver.Models;
+using GraphQLserver.Services;
+
+namespace GraphQLserver.GraphQL;
+
+public class CountryResolvers
+{
+  public IQueryable<Venue> GetVenues([Parent] Country country, [Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+  {
+    return context.Venues
+                  .Where(v => v.VenueId == tenantIdResolver.TenantId); 
+  }
+}
---
+++ GraphQL/CountryType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQLserver.Models;
+
+namespace GraphQLserver.GraphQL
+{
+  public class CountryType : ObjectType<Country>
+  {
+    protected override void Configure(IObjectTypeDescriptor<Country> descriptor)
+    {
+      descriptor.Field(c => c.Venues)
+          .ResolveWith<CountryResolvers>(r => r.GetVenues(default, default!, default))
+          .UseFiltering()
+          .UseSorting();
+    }
+  }
+}
