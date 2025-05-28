3b8034fb370083d8a4eda4b660b78bdbe87afc5d
mGolec73 <mateja.golec@fer.hr>
2025-05-28 22:04:32 +0200

    using Projections

--- GraphQL/Queries.cs
+++ GraphQL/Queries.cs
@@ -41,80 +41,69 @@ namespace GraphQLserver.GraphQL
     }
 
 
-    [GraphQLDescription("Retrieves a list of customers, including details about their country, ticket purchases, and associated venue.")]
-    [UseFiltering]
-    [UseSorting]
-    public IQueryable<Customer> GetCustomers([Service] BMProjekt2024Context context) => context.Customers
-        .Include(e => e.CountryCodeNavigation)
-        .Include(e => e.TicketPurchases)
-        .Include(e => e.Venue);
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Customer> GetCustomers([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
+            var tenantId = tenantIdResolver.TenantId;
+            return context.Customers.Where(cu => cu.VenueId == tenantId);
+        }
 
 
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<EventSection> GetEventSections([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
+            var tenantId = tenantIdResolver.TenantId;
+            return context.EventSections.Where(es => es.VenueId == tenantId);
+        }
 
-    [GraphQLDescription("Retrieves a list of event sections with support for filtering and sorting, including details about the event, section, and associated tickets.")]
-    [UseFiltering]
-    [UseSorting]
-    public IQueryable<EventSection> GetEventSections([Service] BMProjekt2024Context context) => context.EventSections
-        .Include(e => e.Event)
-        .Include(e => e.Section)
-        .Include(e => e.Tickets);
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Event> GetEvents([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
+            var tenantId = tenantIdResolver.TenantId;
+            return context.Events.Where(e => e.VenueId == tenantId); ;
+        }
 
-   
-    [UseFiltering]
-    [UseSorting]
-    [GraphQLDescription("Retrieves a list of events with support for filtering and sorting, including associated event sections and venue data.")]
-    public IQueryable<Event> GetEvents([Service] BMProjekt2024Context context) => context.Events
-        .Include(e => e.EventSections)
-        .Include(e => e.Venue);
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Section> GetSections([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
+            var tenantId = tenantIdResolver.TenantId;
+            return context.Sections.Where(s => s.VenueId == tenantId);
+        }
 
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Ticket> GetTickets([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
+            var tenantId = tenantIdResolver.TenantId;
+            return context.Tickets.Where(t => t.VenueId == tenantId);
+        }
 
-  
-    [UseFiltering]
-    [UseSorting]
-    [GraphQLDescription("Retrieves a list of sections with support for filtering and sorting, including associated event sections and venue data.")]
-    public IQueryable<Section> GetSections([Service] BMProjekt2024Context context) => context.Sections
-        .Include(e => e.EventSections)
-        .Include(e => e.Venue);
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<VenueType_> GetVenueTypes([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
 
+            var tenantId = tenantIdResolver.TenantId;
+            return context.VenueTypes.Where(c => c.Venues.Any(v => v.VenueId == tenantId)); ;
+        }
 
-    [UseFiltering]
-    [UseSorting]
-    [GraphQLDescription("Retrieves a list of tickets with support for filtering and sorting, including associated event sections and ticket purchase data.")]
-    public IQueryable<Ticket> GetTickets([Service] BMProjekt2024Context context) => context.Tickets
-        .Include(e => e.EventSections)
-        .Include(e => e.TicketPurchases);
-
-
-    
-    [UseFiltering]
-    [UseSorting]
-    [GraphQLDescription("Retrieves a list of venue types with support for filtering and sorting, including associated venues.")]
-    public IQueryable<VenueType_> GetVenueTypes([Service] BMProjekt2024Context context) => context.VenueTypes
-        .Include(e => e.Venues);
-
-
-    [UseFiltering]
-    [UseSorting]
-    [GraphQLDescription("Retrieves a list of venues with support for filtering and sorting, including details about the country, associated customers, events, sections, and venue type.")]
-    public IQueryable<Venue> GetVenues([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
-    {
-      var tenantId = tenantIdResolver.TenantId;
-      if (tenantId == null)
-      {
-        Console.WriteLine("TenantId not found");
-      }
-      else
-      {
-        Console.WriteLine($"TenantId found: {tenantId}");
-      }
-
-      return context.Venues
-      .Include(e => e.CountryCodeNavigation)
-      .Include(e => e.Customers)
-      .Include(e => e.Events)
-      .Include(e => e.Sections)
-      .Include(e => e.VenueTypeNavigation)
-      .Where(e => e.VenueId == tenantId);
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Venue> GetVenues([Service] BMProjekt2024Context context, [Service] ITenantIdResolverService tenantIdResolver)
+        {
+            var tenantId = tenantIdResolver.TenantId;
+            return context.Venues.Where(e => e.VenueId == tenantId);
+        }
     }
-  }
 }
